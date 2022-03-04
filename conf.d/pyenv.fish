ac78f2fcd80c379bb3afdf11509c56773018800f
catLee <catlee@plurk.com>
2022-03-04 15:14:45 +0800

    update

---
+++ conf.d/pyenv.fish
@@ -0,0 +1,4 @@
+export PATH="/home/catlee/.pyenv/bin:$PATH"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/shims:$PATH"
+pyenv init - | source
--- config.fish
+++ config.fish
@@ -1,11 +1,20 @@
 export PATH="$HOME/.poetry/bin:$PATH"
-export PATH="/home/catlee/.pyenv/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/local/bin:$PATH"
 export PATH="$HOME/.bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 
+export LSP_USE_PLISTS=true
+
+set -gx  LC_ALL en_US.UTF-8
+
 starship init fish | source
-pyenv init - | source
-pyenv virtualenv-init - | source
-sh $NVM_DIR/nvm.sh
+set nvm_default_version 16
+bass source $NVM_DIR/nvm.sh
+
+set pipenv_fish_fancy yes
+
+# libcurl
+#export LD_LIBRARY_PATH="$HOME/.local/lib:$LD_LIBRARY_PATH"
+#export PKG_CONFIG_PATH="$HOME/.local/lib/pkgconfig:$PKG_CONFIG_PATH"
